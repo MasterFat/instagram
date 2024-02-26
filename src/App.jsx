6ef0e087ace170302ca56c16bd8fcc1ff1defb34
MasterFat <sd67fg89@gmail.com>
2024-02-26 16:23:08 +0800

    新增: 1.customHook  "useBoolean" 以控制彈出頁面的狀態 刪除: 1.newComment  useState (留言輸入) 2.showPost  useState (貼文頁面開關) 3.showStorys  useState (限動頁面開關) 修改: 1.利用 "useBoolean"控制限時動態頁面開關(簡化傳入的props) 2.利用 "useBoolean"控制彈出的當則貼文頁面開關(簡化傳入的props)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useCallback } from "react"
 import "./styles.css"
 
 //components
@@ -7,25 +7,40 @@ import { LeftSideBar } from "./LeftSideBar/LeftSideBar"
 import { Posts } from "./Posts/Posts"
 import { RightSideContent } from "./RightSideContent/RightSideContent"
 import { Slick } from "./SlideBar/Slick"
-import { Storys } from "./SlideBar/Storys"
+import { Stories } from "./SlideBar/Stories"
+
+const useBoolean = (init) => {
+  const [state, setState] = useState(init)
+
+  const close = useCallback(() => {
+    setState(false)
+  }, [])
+
+  const open = useCallback(() => {
+    setState(true)
+  }, [])
+
+  const toggle = useCallback(() => {
+    setState((state) => !state)
+  }, [])
+
+  return { state, open, close, toggle }
+}
 
 export default function App() {
-  //新增貼文留言輸入字串
-  const [newComment, setNewComment] = useState("")
-
-  //彈出每一則貼文頁面的開關
-  const [showPosts, setShowPosts] = useState(false)
-
   //設定按下 "查看所有留言" 或 "留言圖按鈕" 的當則貼文ID
   const [postId, setpostId] = useState("")
-
-  //彈出限動頁面開關
-  const [showStorys, setShowStorys] = useState(false)
 
   //設定按下的限動ID
   const [storyId, setStoryId] = useState("")
 
   const [spin, setSpin] = useState(false)
+
+  //限時動態頁面開關
+  const storiesVisible = useBoolean(false)
+
+  //當則貼文內容頁面開關
+  const eachPostVisible = useBoolean(false)
 
   return (
     <div className="w-full h-full flex justify-items-center relative">
@@ -33,18 +48,18 @@ export default function App() {
       <LeftSideBar />
 
       {/*限時動態頁面(按下上方"任一個限時動態"時才彈出)*/}
-      {showStorys && <Storys showStorys={showStorys} setShowStorys={setShowStorys} storyId={storyId} setStoryId={setStoryId} spin={spin} setSpin={setSpin} />}
+      {storiesVisible.state && <Stories close={storiesVisible.close} storyId={storyId} setStoryId={setStoryId} spin={spin} setSpin={setSpin} />}
 
       {/*每則貼文頁面(按下"留言圖示"或"查看全部留言"才彈出)*/}
-      {showPosts && <EachPost postId={postId} showPosts={showPosts} setShowPosts={setShowPosts} newComment={newComment} setNewComment={setNewComment} />}
+      {eachPostVisible.state && <EachPost close={eachPostVisible.close} postId={postId} />}
 
       <div className="w-full h-full flex flex-wrap justify-center relative mt-12 md:mt-0 md:ml-16 xl:ml-48">
         <div className="w-full md:w-[36rem] h-full flex flex-wrap justify-center">
           {/*上方限時動態區塊*/}
-          <Slick showStorys={showStorys} setShowStorys={setShowStorys} setStoryId={setStoryId} spin={spin} setSpin={setSpin} />
+          <Slick openStories={storiesVisible.open} setStoryId={setStoryId} spin={spin} setSpin={setSpin} />
 
           {/*中間貼文區塊*/}
-          <Posts setpostId={setpostId} showPosts={showPosts} setShowPosts={setShowPosts} newComment={newComment} setNewComment={setNewComment} />
+          <Posts openEachPost={eachPostVisible.open} setpostId={setpostId} />
         </div>
 
         {/*右側 "為您推薦" 內容*/}
