04c92ded7272a70005e496fb3e098ee834b75438
MasterFat <sd67fg89@gmail.com>
2024-02-26 17:41:09 +0800

    第59行改為第71行

--- src/LeftSideBar/LeftSideBar.jsx
+++ src/LeftSideBar/LeftSideBar.jsx
@@ -4,36 +4,77 @@ import { useState } from "react"
 import Build from "./buttonComponents/build"
 import { More } from "./buttonComponents/More"
 import { Search } from "./buttonComponents/Search"
-import { Notify } from "./buttonComponents/Notify"
+import { MobileSidebar } from "./MobileSidebar"
+
+//側欄按鈕靜態資料
+import { LeftSideBarData } from "./lib/LeftSideBarData"
 
 //icons
 import { FaInstagram } from "react-icons/fa"
+import { Notify } from "./buttonComponents/Notify"
 import instagram from "/src/image/instagram.svg"
 
-//LeftSideBar icons
-import { FaRegCompass } from "react-icons/fa"
-import { BiMoviePlay } from "react-icons/bi"
-import { PiMessengerLogoBold } from "react-icons/pi"
-import { FaRegHeart } from "react-icons/fa6"
-import { FiPlusSquare } from "react-icons/fi"
-import { FaRegUser } from "react-icons/fa"
-import { FaThreads } from "react-icons/fa6"
-import { FaBars } from "react-icons/fa6"
-import { GoHomeFill } from "react-icons/go"
-import { PiMagnifyingGlassBold } from "react-icons/pi"
-
 export const LeftSideBar = () => {
-  //設定按下的按鈕id
-  const [currentId, setcurrentId] = useState(-1) // TODO: 用css做
+  //設定滑鼠移進移出每個按鈕的id
+  const [currentId, setcurrentId] = useState(-1)
 
+<<<<<<< HEAD
   //判斷按下的按鈕是哪個
   const handleButtonClick = (item) => {
     setcurrentId(item.id)
     if (currentId === item.id) {
       close()
+=======
+  //"建立"按鈕的開關
+  const [showMask, setShowMask] = useState(false)
+
+  //"搜尋"按鈕的開關
+  const [showSearch, setShowSearch] = useState(false)
+
+  //"通知"按鈕的開關
+  const [showNotify, setShowNotify] = useState(false)
+
+  //"更多"按鈕的開關
+  const [showMore, setShowMore] = useState(false)
+
+  //判斷按下的按鈕是哪個
+  const handleButtonClick = (item) => {
+    if (item === "建立") {
+      setShowMask(!showMask)
+>>>>>>> parent of e7bd326 (刪除:)
       return
     }
-    if (!item.component) {
+    if (item === "更多") {
+      setShowMore(!showMore)
+      return
+    }
+    if (item === "搜尋") {
+      setShowSearch(!showSearch)
+      setShowNotify(false)
+      return
+    }
+    if (item === "探索") {
+      alert("功能尚未啟用")
+      return
+    }
+    if (item === "連續短片") {
+      alert("功能尚未啟用")
+      return
+    }
+    if (item === "訊息") {
+      alert("功能尚未啟用")
+      return
+    }
+    if (item === "通知") {
+      setShowNotify(!showNotify)
+      setShowSearch(false)
+      return
+    }
+    if (item === "個人檔案") {
+      alert("功能尚未啟用")
+      return
+    }
+    if (item === "Threads") {
       alert("功能尚未啟用")
       return
     }
@@ -67,16 +108,25 @@ export const LeftSideBar = () => {
 
   return (
     <>
-      {/*顯示按下按鈕的內容*/}
-      <div>{LeftSideBarData.map((item) => currentId === item.id && <div key={item.id}>{item.component}</div>)}</div>
+      {/*跳出"建立"畫面*/}
+      {showMask && <Build showMask={showMask} setShowMask={setShowMask} />}
+
+      {/*彈出"更多"列表*/}
+      {showMore && <More />}
+
+      {/*滑出"搜尋"框*/}
+      <Search showSearch={showSearch} setShowSearch={setShowSearch} />
+
+      {/*滑出"通知"框*/}
+      <Notify showNotify={showNotify} />
 
       {/*手機板上下欄選單(當畫面寬度<768時顯示)*/}
-      <MobileSidebar handleButtonClick={handleButtonClick} LeftSideBarData={LeftSideBarData} />
+      <MobileSidebar handleButtonClick={handleButtonClick} />
 
       {/*電腦版左側欄選單(當畫面寬度<1280時簡化，當畫面寬度<768時隱藏)*/}
       <div
         className={`${
-          currentId === 2 || currentId === 6 ? "w-16 xl:w-16" : "w-16 xl:w-48 border-r"
+          showSearch || showNotify ? "w-16 xl:w-16" : "w-16 xl:w-48 border-r"
         } hidden md:block h-full fixed z-40 left-0 border-r-zinc-800 flex-col justify-center transition-all ease-out duration-500`}
       >
         {/*IG字母與LOGO*/}
@@ -85,16 +135,16 @@ export const LeftSideBar = () => {
             <a
               href=""
               className={`${
-                currentId === 2 || currentId === 6 ? "w-0 opacity-0" : "w-0 opacity-0 xl:opacity-100 lg:w-full lg:h-full"
-              } overflow-hidden flex items-center transition-all ease-out duration-100`}
+                showSearch || showNotify ? "w-0 opacity-0" : "w-0 opacity-0 xl:opacity-100 lg:w-full lg:h-full"
+              } overflow-hidden transition-all flex items-center ease-out duration-100`}
             >
               <img src={instagram} alt="instagram" />
             </a>
             <a
               href=""
               className={`${
-                currentId === 2 || currentId === 6 ? "w-full h-full" : "xl:w-0 xl:h-0"
-              } absolute flex justify-center items-center transition-all ease-out duration-100`}
+                showSearch || showNotify ? "w-full h-full" : "xl:w-0 xl:h-0"
+              } flex justify-center items-center transition-all ease-out duration-100 absolute`}
             >
               <FaInstagram className="text-xl" />
             </a>
@@ -106,61 +156,26 @@ export const LeftSideBar = () => {
           <ul className="flex-col w-11/12">
             {LeftSideBarData.map((item) => (
               <li
+                onMouseEnter={() => setcurrentId(item.id)}
+                onMouseLeave={() => setcurrentId(-1)}
                 key={item.id}
                 className={`${
-                  item.child ? "mt-10" : "" //從"Threads"隔開一段距離
-                } group flex w-full text-white h-1/12 border-0 rounded-lg py-1.5 my-1.5 hover:bg-zinc-900 transition-all ease-out duration-300`}
+                  item.child && "mt-10" //從"Threads"隔開一段距離
+                } flex w-full text-white h-1/12 border-0 rounded-lg py-1.5 my-1.5 hover:bg-zinc-900 transition-all ease-out duration-300`}
               >
                 <button
-                  onClick={() => handleButtonClick(item)}
-                  className={`${currentId === 2 || currentId === 6 ? "" : "xl:justify-normal"} w-full h-full flex justify-center`}
+                  onClick={() => handleButtonClick(item.name)}
+                  className={`${showSearch || showNotify ? "" : "xl:justify-normal"} w-full h-full flex justify-center`}
                 >
-                  <div className={`group-hover:scale-105 flex items-center h-6 justify-center text-xl transition-all`}>{item.icon}</div>
-                  <div className={`${currentId === 2 || currentId === 6 ? "hidden" : "hidden xl:block"} ml-3`}>
-                    <span className="text-m w-20 h-6 flex items-center">{item.name}</span>
+                  <div className={`${currentId === item.id && "scale-105"} flex items-center h-6 justify-center text-xl transition-all`}>{item.icon}</div>
+                  <div className={`${showSearch || showNotify ? "hidden" : "hidden xl:block"} ml-3`}>
+                    <span className="text-m h-6 flex items-center justify-center">{item.name}</span>
                   </div>
                 </button>
               </li>
             ))}
           </ul>
         </nav>
-      </div>
-    </>
-  )
-}
-
-const MobileSidebar = ({ handleButtonClick, LeftSideBarData }) => {
-  const iconsJudgment = (item) => {
-    if (item.id === 2) {
-      return true
-    }
-    if (item.id === 6) {
-      return true
-    }
-    if (item.id === 9) {
-      return true
-    }
-    if (item.id === 10) {
-      return true
-    }
-  }
-  return (
-    <>
-      <div className="w-full h-12 bg-black fixed z-40 top-0 flex justify-center text-white border-b border-zinc-800 md:hidden">
-        <div className="w-11/12 h-full flex justify-between items-center">
-          <img src={instagram} alt="instagram" />
-          <input id="input2" type="text" placeholder="   搜尋" className="w-1/2 h-8 bg-zinc-800 outline-none border-0 rounded-md" />
-          <FaRegHeart />
-        </div>
-      </div>
-      <div className="w-full h-12 bg-black fixed z-40 bottom-0 flex justify-center items-center text-white border-t border-zinc-800  md:hidden">
-        <div className="w-10/12 flex justify-between text-xl">
-          {LeftSideBarData.map((item) => (
-            <div key={item.id} className={`${iconsJudgment(item) ? "hidden" : "block"}`}>
-              <button onClick={() => handleButtonClick(item)}>{item.icon}</button>
-            </div>
-          ))}
-        </div>
       </div>
     </>
   )
